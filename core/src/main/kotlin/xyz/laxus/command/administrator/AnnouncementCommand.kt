1aff32d8fb29b7207dd5cd43905d7584ebf6262b
TheMonitorLizard <kaidangustave@yahoo.com>
2018-05-22 16:23:25 -0500

    Added ability to attach files to tags and custom commands

--- core/src/main/kotlin/xyz/laxus/command/administrator/AnnouncementCommand.kt
+++ core/src/main/kotlin/xyz/laxus/command/administrator/AnnouncementCommand.kt
@@ -13,8 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-@file:Suppress("unused") // TODO
-
 package xyz.laxus.command.administrator
 
 import net.dv8tion.jda.core.Permission
